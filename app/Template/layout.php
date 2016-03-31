716078e30a62c9e51764df251e0d8e0b36cc88de
caspanh <karen_caspan04@hotmail.com>
2016-03-31 11:19:15 -0600

    arreglo layout

--- app/Template/layout.php
+++ app/Template/layout.php
@@ -18,27 +18,33 @@
 
         <?= $this->asset->colorCss() ?>
         <?= $this->asset->css('assets/css/app.css') ?>
+       
         <?= $this->asset->css('assets/css/print.css', true, 'print') ?>
         <?= $this->asset->customCss() ?>
 
         <?= $this->hook->asset('css', 'template:layout:css') ?>
         <?= $this->hook->asset('js', 'template:layout:js') ?>
 
-        <link rel="icon" type="image/png" href="<?= $this->url->dir() ?>assets/img/favicon.png">
+        <link rel="icon" type="image/png" href="<?= $this->url->dir() ?>assets/img/logo11.png">
         <link rel="apple-touch-icon" href="<?= $this->url->dir() ?>assets/img/touch-icon-iphone.png">
         <link rel="apple-touch-icon" sizes="72x72" href="<?= $this->url->dir() ?>assets/img/touch-icon-ipad.png">
         <link rel="apple-touch-icon" sizes="114x114" href="<?= $this->url->dir() ?>assets/img/touch-icon-iphone-retina.png">
         <link rel="apple-touch-icon" sizes="144x144" href="<?= $this->url->dir() ?>assets/img/touch-icon-ipad-retina.png">
+        <link href='https://fonts.googleapis.com/css?family=ABeeZee' rel='stylesheet' type='text/css'>
 
         <title><?= isset($title) ? $this->e($title) : 'Kanboard' ?></title>
 
         <?= $this->hook->render('template:layout:head') ?>
     </head>
-    <body data-status-url="<?= $this->url->href('app', 'status') ?>"
+    
+    <body font-family="ABeeZee"
+          data-status-url="<?= $this->url->href('app', 'status') ?>"
           data-login-url="<?= $this->url->href('auth', 'login') ?>"
           data-markdown-preview-url="<?= $this->url->href('TaskHelper', 'preview') ?>"
           data-timezone="<?= $this->app->getTimezone() ?>"
           data-js-lang="<?= $this->app->jsLang() ?>">
+
+
 
     <?php if (isset($no_layout) && $no_layout): ?>
         <?= $content_for_layout ?>
